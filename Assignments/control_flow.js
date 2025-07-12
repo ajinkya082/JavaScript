670e462b24611fc5a9d8684fca2ef01412595121
ajinkya082 <ajinkyaraut0804@gmail.com>
2025-07-12 21:17:32 +0530

    Add files via upload

---
+++ Assignments/control_flow.js
@@ -0,0 +1,16 @@
+// Assignment 1: Write a program using if-else to check voting eligibility based on 
+// age
+
+let age=parseInt(prompt("Enter your age"));
+if(age>18){
+    console.log("Eligible for vote")
+}else{
+    console.log("Not eligible")
+}
+
+//  Assignment 2: Use a for loop to print multiplication table of 7.
+ 
+const no=7;
+for(let i=1;i<=10;i++){
+    console.log(i*no);
+}
